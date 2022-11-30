3a5430fc27cd06e00789ce991f003da32bffdf85
ambrojessica <ambrojessica0@gmail.com>
2022-11-29 21:50:59 -0800

    switching to styled componenets

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
